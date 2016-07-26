c514a0cac7d78634f8dca9472379a25511569171
loveru <252431193@qq.com>
2016-07-26 21:18:57 +0800

    hjc

--- app/src/main/java/com/example/administrator/mysnack/fragment/MineFragment.java
+++ app/src/main/java/com/example/administrator/mysnack/fragment/MineFragment.java
@@ -15,16 +15,15 @@ import com.example.administrator.mysnack.R;
 public class MineFragment extends Fragment {
 
 
-    public MineFragment() {
-        // Required empty public constructor
-    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        View view = inflater.inflate(R.layout.fragment_mine, container, false);
+
+        return view;
     }
 
 }
